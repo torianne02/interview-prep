1af18cb128530c57d0fc5eefb1ced32a55399ae6
torianne02 <victoria.fluharty@gmail.com>
2020-05-19 18:01:14 -0700

    adds built from scratch code for binary tree in JS

---
+++ data-structures/javascript/binary-search-tree.js
@@ -0,0 +1,100 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        } 
+      }
+    }
+    return this;
+  }
+
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value === currentNode.value) {
+        return currentNode;
+      } else if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
+  }
+
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+
+    let currentNode = this.root;
+    let parentNode = null;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        
+      }
+    }
+  }
+}
+
+const myTree = new BinarySearchTree();
+// console.log(myTree);
+myTree.insert(9);
+myTree.insert(4);
+myTree.insert(6);
+myTree.insert(20);
+myTree.insert(170);
+// console.log(myTree)
+myTree.insert(15);
+myTree.insert(1);
+myTree.lookup(20);
+myTree.lookup(77);
+// console.log(JSON.stringify(traverse(myTree.root)))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+
+  tree.right = node.right === null ? null : traverse(node.right);
+
+  return tree;
+}
