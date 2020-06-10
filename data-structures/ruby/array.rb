89e70c7cdb09351b4451183a239a5659a39e9b0e
torianne02 <victoria.fluharty@gmail.com>
2020-06-10 15:06:59 -0700

    adds new methods and renames methods for data-structures/ruby/array.rb

--- data-structures/ruby/array.rb
+++ data-structures/ruby/array.rb
@@ -1,12 +1,12 @@
 class MyArray
-  attr_reader :length, :data
+  attr_reader :length
 
   def initialize
-    @length = 0;
-    @data = {};
+    @length = 0
+    @data = {}
   end
 
-  def get(index) 
+  def at(index) 
     @data[index]
   end 
 
@@ -22,12 +22,21 @@ class MyArray
     return lastItem
   end 
 
-  def delete(index)
+  def delete_at(index)
     item = @data[index]
 
     self.shift_items(index)
     return item
   end
+
+  def delete(item)
+    @data.each_value.with_index do |value, index|
+      if value === item
+        self.shift_items(index)
+      end 
+    end
+    return item
+  end 
 
   def shift_items(index)
     counter = index
@@ -40,6 +49,12 @@ class MyArray
     @length -= 1
   end
 
+  def includes?(item)
+    result = false
+    @data.each_value { |value| result = true if value === item }
+    return result
+  end 
+
   def print
     @data.each_value do |value|
       p value
@@ -50,13 +65,11 @@ end
 my_array = MyArray.new()
 my_array.push(3)
 my_array.push(5)
-# print my_array.data
-# my_array.get(1)
+my_array.push(8)
+# my_array.at(1)
 # my_array.pop()
-# my_array.length
-# print my_array.data
-# my_array.delete(0)
-# print my_array.data
-# my_array.length
+# my_array.delete_at(0)
+# print my_array.delete(5)
+print my_array.includes?(9)
 
-my_array.print()
+# print my_array.print()
