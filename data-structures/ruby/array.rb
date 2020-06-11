cc2395fe19c876e7362e293e963288c0d2ae3879
torianne02 <victoria.fluharty@gmail.com>
2020-06-10 17:35:02 -0700

    updates variable names in ruby/array.rb

--- data-structures/ruby/array.rb
+++ data-structures/ruby/array.rb
@@ -3,60 +3,72 @@ class MyArray
 
   def initialize
     @length = 0
-    @data = {}
+    @elements = {}
   end
 
-  def at(index) 
-    @data[index]
+  def push(element)
+    @elements[@length] = element
+    @length += 1
   end 
 
-  def push(item)
-    @data[@length] = item 
-    @length += 1
-  end
+  def at(index)
+    if index < @length
+      @elements.each_value.with_index { |value, i| return value if i == index }
+    else 
+      return nil
+    end
+  end 
 
   def pop()
-    lastItem = @data[@length - 1]
-    @data.delete(@length - 1)
+    last_element = @elements[@length - 1]
+    @elements.delete(@length - 1)
     @length -= 1
-    return lastItem
+
+    return last_element
   end 
 
-  def delete_at(index)
-    item = @data[index]
-
-    self.shift_items(index)
-    return item
-  end
-
-  def delete(item)
-    @data.each_value.with_index do |value, index|
-      if value === item
-        self.shift_items(index)
-      end 
-    end
-    return item
-  end 
-
-  def shift_items(index)
+  def shift_elements(index)
     counter = index
     while counter < @length - 1 do
-      @data[counter] = @data[counter + 1]
+      @elements[counter] = @elements[counter + 1]
       counter += 1
     end 
 
-    @data.delete(@length - 1)
+    @elements.delete(@length - 1)
     @length -= 1
   end
 
-  def includes?(item)
+  def delete_at(index)
+    if index < @length
+      element = @elements[index]
+      self.shift_elements(index)
+
+      return element
+    else 
+      return nil
+    end 
+  end 
+
+  def delete(element)
+    @elements.each_value.with_index do |value, index|
+      if value == element
+        self.shift_elements(index)
+      elsif index == @length - 1
+        return nil
+      end 
+    end 
+
+    return element
+  end
+
+  def includes?(element)
     result = false
-    @data.each_value { |value| result = true if value === item }
+    @elements.each_value { |value| result = true if value == element }
     return result
   end 
 
   def print
-    @data.each_value do |value|
+    @elements.each_value do |value|
       p value
     end
   end
@@ -66,10 +78,10 @@ my_array = MyArray.new()
 my_array.push(3)
 my_array.push(5)
 my_array.push(8)
-# my_array.at(1)
+# print my_array.at(1)
 # my_array.pop()
-# my_array.delete_at(0)
+print my_array.delete(8)
 # print my_array.delete(5)
-print my_array.includes?(9)
+# print my_array.includes?(9)
 
 # print my_array.print()
